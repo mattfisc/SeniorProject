ac12c9bf185432e4eb1bfb6d1d80058cadae4808
Matthew Fischer <mattfisc@gmail.com>
2020-09-01 09:13:10 -0700

    php query returning row number

--- booklist.php
+++ booklist.php
@@ -5,12 +5,37 @@
 // echo parse_url($url, search); # output "myqueryhash"
 
 $book = $_GET['q'];
-echo $book
 
-//NOTE: $mysqli = new mysqli("127.0.0.1", "username", "password", "database", 3306);
-// $mysqli = new mysqli("localhost","root","","booklisting");
-// if ($mysqli->connect_errno) {
-//   echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-// }
+
+//WEBPAGE NOTE: $mysqli = new mysqli("127.0.0.1", "username", "password", "database", 3306);
+$conn = new mysqli("localhost", "root", "", "cs450");
+if ($conn->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+
+// STRING TEST
+$test = "SELECT * FROM `booklisting` WHERE title = '$book' ";
+
+
+//  PERFORM QUERY
+if ($result = mysqli_query($conn, $test)) {
+
+    // ROW NUMBER
+    $num_results = mysqli_num_rows($result);
+
+    // FOUND TRUE
+    if(mysqli_num_rows($result) > 0){
+
+    }else{// FALSE
+
+    }
+
+    // FREES MEMORY
+    mysqli_free_result($result);
+  }
+  
+mysqli_close($conn);
+
 
 ?>
