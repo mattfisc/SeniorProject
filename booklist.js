9db4376c71314c1e3f444092d28b87893785e633
Matthew Fischer <mattfisc@gmail.com>
2020-09-03 11:20:02 -0700

    text not json need json and send to php

--- booklist.js
+++ booklist.js
@@ -3,57 +3,57 @@
 // INPUT VALIDATED BY REGULAR EXPRESSION
 // ERROR OUTPUT BY WINDOW ALERT
 function searchforbook(){ 
-    var text = '{';
-    var status = 1;// status one means no errors -1 is error found
+    var text = '';
+    var state = 1;// ERROR STATE
 
     // TITLE CHECK
     if(!document.getElementById("title_input").value.length==0){
         var word = document.getElementById("title_input").value.toLowerCase();
         if(!/[^a-z ]/.test(word))// ERROR TITLE... LETTERS ONLY
-            text += '{"title":"' + document.getElementById("title_input").value +'"},';
+            text += '"title":"' + document.getElementById("title_input").value +'"';
         else
-            status = -1;
+            state = -1;
     }
     else
-        text += '{"title":"empty"},';
+        text += '"title":"empty"';
 
     // AUTHOR CHECK
     if(!document.getElementById("author_input").value.length == 0){
         var word = document.getElementById("author_input").value.toLowerCase();
         if(!/[^a-z ]/.test(word))// ERROR AUTHOR.. LETTERS ONLY
-            text += '{"author":"' + document.getElementById("author_input").value +'"},';
+            text += ',"author":"' + document.getElementById("author_input").value +'"';
         else
-            status = -2;
+            state = -2;
     }
     else
-        text += '{"author":"empty" },';
+        text += '"author":"empty"';
 
     // ISBN CHECK
     if(!document.getElementById("isbn_input").value.length==0){
         var word = document.getElementById("isbn_input").value;
         if(!/[^0-9]/.test(word))// ERROR ISBN...NUMBERS ONLY
-            text += '{"isbn":' + document.getElementById("isbn_input").value +'},';
+            text += ',"isbn":' + document.getElementById("isbn_input").value +'';
         else
-            status = -3;
+            state = -3;
     }
     else
-        text += '{"isbn":"empty"},';
+        text += '"isbn":"empty"';
 
     // LOCATION CHECK
     if(!document.getElementById("location_input").value.length==0){
         var word = document.getElementById("location_input").value.toLowerCase();
         if(!/[^a-z ]/.test(word))// ERRORS LOCATION.. LETTERS ONLY
-            text += '{ "location":"' + document.getElementById("location_input").value +'" },';
+            text += ',"location":"' + document.getElementById("location_input").value +'"';
         else
-            status = -4;
+            state = -4;
     }
     else
-        text += '{"location":"empty"},';
-
+        text += '"location":"empty"';
+    //text += '}';
 
 
     // ERROR
-    switch(status){
+    switch(state){
         case -1:
             window.alert("ERROR Title... no numbers or symbols allowed");
         case -2:
@@ -67,9 +67,10 @@ function searchforbook(){
     }
 
 
-    window.alert(text);
-    var json = JSON.parse(text);
     
+    var json = JSON.parse(text);
+    window.alert(text);
+
     // var xhr = new XMLHttpRequest();
     // xhr.onreadystatechange = createList;
     // xhr.open("GET", "booklist.php?title=" + title_input, true); 
