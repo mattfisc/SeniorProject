6f7d6552a1a3b6d244843eac94a3125222bebbb6
Matthew Fischer <mattfisc@gmail.com>
2020-09-20 01:30:00 -0700

    home onload function in process
    user class is started

--- home.js
+++ home.js
@@ -1,10 +1,30 @@
+window.onload = function(){
+
+    const profile = new User_Member();
+    // SET NAME TO NAV BAR
+    var element = document.getElementById("pName").innerHTML = "Guest";
+}
+
+
 class User_Member{
 
+    constructor(){
+        this.name = "Guest";
+    }
+
+    constructor(name){
+        this.name = name;
+    }
     // 
 
     // Getter profile
+    getProfileName(){
+        return this.name;
+    }
 
     // setter profile
-
+    setProfileName(name){
+        this.name = name;
+    }
 
 }
