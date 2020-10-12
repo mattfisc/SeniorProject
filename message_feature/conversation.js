100bfa1bd2d6c5d3e530ec3f58b75dcfbc2645ad
Matthew Fischer <mattfisc@gmail.com>
2020-10-12 02:18:10 -0700

    MESSAGE HAS NAME LIST AND IMAGE

--- message_feature/conversation.js
+++ message_feature/conversation.js
@@ -13,22 +13,15 @@ class Conversation{
         this.user1 = user1;
         this.user2 = user2;
         this.bookId = bookId;
-        
+        this.picture = "";
         // ARRAY OF MESSAGE OBJECTS
         this.message_list = [];
     }
 
-    to_string(){
-        var str = "";
-        for (let i = 0; i < message_list.length; i++) {
-            const el = array[i];
-            str += "message: ".concat(el.senderId,el.recieverId,el.message_text,el.timestamp,"\n");
-            
-        }
-        return str;
-    }
+    
     
 }
+
 
 // FIND CONVERSATION
 function findConversation(user1,user2,bookId){
@@ -92,7 +85,7 @@ function set_buyer_list(str){
             const conversation = new Conversation(message.recieverId,message.senderId,message.bookId);
             // ADD MESSAGE TO MESSAGE ARRAY
             conversation.message_list.push(message);
-
+            get_picure(conversation);
             // ADD TO LIST GLOBAL LIST
             conversations_list.push(conversation);
             
@@ -101,76 +94,129 @@ function set_buyer_list(str){
             // ADD MESSAGE TO ALREADY CREATED CONVERSATION
             conv.message_list.push(message);
         }       
-        
     }
-    
 }
 
-function displayMessages(){
+function displayMessages(conversation_obj){
     // DISPLAY CONVERSATIONS
     var div = document.getElementById("output");
 
-    for (let index = 0; index < conversations_list.length; index++) {
-
-        const list = conversations_list[index].message_list;
-
-        for (let i = 0; i < list.length; i++) {
-
-            var bookId = document.createElement('p');
-            var senderId = document.createElement('h3');
-            //var reciever = document.createElement('h3');
-            var text_message = document.createElement('p');
-            var time_stamp = document.createElement('p');
-
-            const m = list[i];
-
-            bookId.innerHTML = m.bookId;
-            senderId.innerHTML = m.senderId;
-            //reciever.innerHTML = m.recieverId;
-            text_message.innerHTML = m.message;
-            time_stamp.innerHTML = m.time_stamp;
-            
-
-            //COL ONE
-
-            // GET BOOK
-            div.appendChild(bookId);
 
 
-            // COL TWO
+    var bookId = document.createElement('p');
+    var senderId = document.createElement('h3');
+    //var reciever = document.createElement('h3');
+    var text_message = document.createElement('p');
+    var time_stamp = document.createElement('p');
 
-            div.appendChild(senderId);  
-            //div.appendChild(reciever);  
-            div.appendChild(text_message);  
-            div.appendChild(time_stamp); 
-            
-        }
+    const obj = conversation_obj.message_list;
+
+    bookId.innerHTML = obj.bookId;
+    senderId.innerHTML = obj.senderId;
+    //reciever.innerHTML = obj.recieverId;
+    text_message.innerHTML = obj.message;
+    time_stamp.innerHTML = obj.time_stamp;
+    
+
+    //COL ONE
+
+    // GET BOOK
+    div.appendChild(bookId);
+
+
+    // COL TWO
+
+    div.appendChild(senderId);  
+    //div.appendChild(reciever);  
+    div.appendChild(text_message);  
+    div.appendChild(time_stamp); 
+    
+    
         
 
 
         
-    }
+    
 }
 
-function emptyList(){
+function clear(){
     // EMPTY OBJECT LIST
     conversations_list = [];
 
     // EMPTY TABLE IN HTML
-    var el = document.getElementById("display");
+    var el = document.getElementById("output");
     el.innerHTML = "";
 }
 
 function displayConversations(){
-    var div = document.getElementById('output');
 
-    var list = document.createElement('ul');
+
+
+    var div = document.getElementById('output');
+    // CLEAR
+    div.innerHTML = "";
+
+
+    
     
     for (let index = 0; index < conversations_list.length; index++) {
         const element = conversations_list[index];
 
+
+        // DIV DETAILS
+        var row = document.createElement('div');
+        row.className = "conversation text-center";
+        row.className = "row";
+
+        // CREATE DIV AND CLASS SIZE BOOTSTRAP
+
+        // LEFT COL
+        var leftcol = document.createElement('div');
+        leftcol.className = "col-xs-12 col-sm-12 col-md-4 col-xl-4";
+        leftcol.style = "text-shadow: 2px 2px black";
+
+        // RIGHT COL
+        var rightcol = document.createElement('div');
+        rightcol.className = "col-xs-12 col-sm-12 col-md-4 col-xl-4";
+        rightcol.style = "text-shadow: 2px 2px black";
         
+
+        // CREATE IMAGE
+        var img = document.createElement('img');
+        var pic_loc = element.picture;
+        var str =  "../upload/".concat( pic_loc );
+        img.src = str;
+        // LEFT COL
+        leftcol.appendChild(img);
+        row.appendChild(leftcol);
         
+
+        // RIGHT COL
+        // CONVERSATION INFORMATION
+        var button = document.createElement('button');
+        button.innerHTML = "CLICK CHAT";
+        rightcol.appendChild(button);
+        //button.onclick = displayMessages();
+        
+        row.appendChild(rightcol);
+        div.appendChild(row);
     }
 
 }
+
+
+function get_picure(obj){
+    var xml_str = "../message_feature/get_picture.php?bookId=".concat(obj.bookId);
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        // NO ERRORS
+        if(this.readyState == 4 && this.status == 200){
+            obj.picture = (this.responseText);
+        }
+    }
+
+    xhr.open("GET",xml_str, true); 
+    xhr.send();
+}
+
